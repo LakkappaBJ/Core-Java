cf14e2fc08833c4a989149e9cf053e20e9033cf1
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-01-26 15:08:23 +0530

    data values checking
    to check primitive data types values without using any letter in float, double, long

---
+++ Check.java
@@ -0,0 +1,15 @@
+//to check primitive data types values without using any letter in float, double, long
+class Check{
+	public static void main(String args[]){
+		float f=20.5F; //f or F
+		float f1=11.22f;
+		double d=30; //optional d or D, f or F
+		double d1=32.2d;
+		double d2=33.255D;
+		double d3=15.25f;// some data loss
+		double d4=54.27F;
+		long l=6363977693L; //l or L
+		System.out.println(f+"\n"+f1+"\n"+d+"\n"+d1+"\n"+d2+"\n"+d3+"\n"+d4+"\n"+l);
+		
+	}
+}
