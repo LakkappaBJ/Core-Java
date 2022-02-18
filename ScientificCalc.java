9e6c951510b488c339668ac745c3366da982d0e1
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-02-18 08:35:51 +0530

    Method Overriding

---
+++ ScientificCalc.java
@@ -0,0 +1,13 @@
+package com.xworkz.javaOops;
+
+public class ScientificCalc extends Calculator {
+	//method overriding
+	
+	public  void addition() {
+		int a=200;
+		int b=300;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	
+}
