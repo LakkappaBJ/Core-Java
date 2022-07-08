6a21b3e02b5881a123508da0d589460588150881
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-07-08 12:25:18 +0530

    weekdays

---
+++ ForLoop-master/WeekDays.java
@@ -0,0 +1,9 @@
+class WeekDays{
+	public static void main(String args[]){
+	String []days={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+	for(int index=0;index<days.length;index=index+1){
+		System.out.println(days[index]);
+	}
+	//System.out.println(days[1]);
+	}
+}
